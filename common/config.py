1071f6a27d476b35eaf709d42da39a6e5e89a984
jin <audwls624@gmail.com>
2023-11-29 01:12:52 +0900

    db connection 추가, 메인 예제 코드 추가

---
+++ common/config.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass, field
+from os import path, environ
+
+base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+
+
+@dataclass
+class Config:
+    """
+    기본 Configuration
+    """
+
+    BASE_DIR: str = base_dir
+    DB_POOL_RECYCLE: int = 900
+    DB_ECHO: bool = True
+    DEBUG: bool = False
+    TEST_MODE: bool = False
+
+
+@dataclass
+class TestConfig(Config):
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+    DB_URL: str = field(default="sqlite:///database/test.db")
+
+
+@dataclass
+class LocalConfig(Config):
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+    DEBUG: bool = True
+    DB_URL: str = field(default="sqlite:///database/local.db")
+
+
+@dataclass
+class ProdConfig(Config):
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+
+
+def get_conf():
+    """
+    환경 불러오기
+    :return:
+    """
+    config = dict(prod=ProdConfig, local=LocalConfig, test=TestConfig)
+    return config[environ.get("API_ENV", "local")]()
--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import uvicorn
-
+from dataclasses import asdict
+from typing import Union
 from fastapi import FastAPI
+from common.config import get_conf
+from fastapi import Depends
+from database.connections import db
+from sqlalchemy.orm import Session
 
+
+env_conf = asdict(get_conf())
 app = FastAPI()
+db.init_app(app, **env_conf)
 
 
 @app.get("/")
 def read_root():
     return "Welcome to Dive Match!"
+
+
+@app.get("/items/{item_id}")
+def read_items(
+    item_id: int, db: Session = Depends(db.session), q: Union[str, None] = None
+):
+    # 여기에서 데이터베이스 세션 'db'를 사용하여 작업 수행
+    print(db)
+    return {"item_id": item_id, "q": q}
 
 
 if __name__ == "__main__":
