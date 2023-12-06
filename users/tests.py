39df810a9065cadafe5eb3df153513195a9bbc6d
myeongjin yang <78643271+audwls624@users.noreply.github.com>
2023-12-06 13:04:21 +0900

    certificate level api 테스트 코드 작성

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from tortoise.contrib.test import finalizer, initializer
-from users.models import Certificate, CertificateName_Pydantic
+from users.models import Certificate, CertificateName_Pydantic, CertificateLevel
 
 
 @pytest.fixture(scope="module")
@@ -20,7 +20,7 @@ def client(initialize_tests):
 
 
 @pytest.fixture(scope="module")
-async def test_data():
+async def test_data_certificates():
     certificates = [
         {"name": "AIDA TEST"},
         {"name": "SSI TEST"},
@@ -35,15 +35,36 @@ async def test_data():
 
 
 @pytest.mark.asyncio
-async def test_certificate_list(client, test_data) -> None:
-    correct_data = await test_data
+async def test_certificate_list(client, test_data_certificates) -> None:
+    correct_data = await test_data_certificates
     response = client.get("/api/user/certificates")
     assert response.status_code == 200
     correct_data_dicts = [
         await CertificateName_Pydantic.from_tortoise_orm(cert) for cert in correct_data
     ]
-    assert response.json() == {
-        "status_code": 200,
-        "message": "Certificates fetched successfully",
-        "data": [cert.dict() for cert in correct_data_dicts],
-    }
+    assert response.json().get("status_code") == 200
+    assert response.json().get("data") == [cert.dict() for cert in correct_data_dicts]
+
+
+@pytest.fixture(scope="module")
+async def test_data_certificate_levels(client):
+    # 테스트 데이터 생성
+    certificate = await Certificate.create(name="Test Certificate")
+    level1 = await CertificateLevel.create(certificate=certificate, level="Level 1")
+    level2 = await CertificateLevel.create(certificate=certificate, level="Level 2")
+
+    return {"certificate": certificate, "levels": [level1, level2]}
+
+
+@pytest.mark.asyncio
+async def test_get_certificate_levels(client, test_data_certificate_levels):
+    certificate_levels = await test_data_certificate_levels
+    certificate_id = certificate_levels["certificate"].id
+    response = client.get(f"/api/user/certificates/{certificate_id}/levels")
+    assert response.status_code == 200
+
+    data = response.json()
+    assert data["status_code"] == 200
+    assert len(data["data"]) == 2
+    assert data["data"][0]["level"] == "Level 1"
+    assert data["data"][1]["level"] == "Level 2"
